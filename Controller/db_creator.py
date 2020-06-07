3fa9ea9d1d291bfb36ed35a9a4af3e979c9349a6
Zenahr Barzani <zenmatica@gmail.com>
2020-06-07 21:39:38 +0200

    updated database creation script

--- Controller/db_creator.py
+++ Controller/db_creator.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully")
 
-conn.execute('CREATE TABLE weights (ID INTEGER PRIMARY KEY AUTOINCREMENT, WEIGHT DECIMAL NOT NULL, DATE_TIME TEXT NOT NULL)')
+conn.execute('CREATE TABLE weights (ID INTEGER PRIMARY KEY AUTOINCREMENT, WEIGHT DECIMAL NOT NULL, DATE_TIME INTEGER UNIQUE NOT NULL)')
 print ("Table created successfully")
 conn.close()
