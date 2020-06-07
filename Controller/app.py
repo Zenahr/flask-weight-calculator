993b298df85766270b8f5c01860a06d896751cca
Zenahr Barzani <zenmatica@gmail.com>
2020-06-07 15:03:44 +0200

    added custom templates path

---
+++ Controller/app.py
@@ -0,0 +1,25 @@
+import os
+import webbrowser
+from threading import Timer
+from flask import Flask, render_template, url_for
+
+app=Flask(__name__)
+PORT="8080"
+TEMPLATES_PATH=os.path.join(os.getcwd(), "View\\templates")
+# print(TEMPLATES_PATH)
+
+@app.route('/')
+def index(name=None):
+    return render_template('index.html', template_folder=TEMPLATES_ROOT)
+
+
+
+
+
+# def open_browser():
+      # webbrowser.open_new('http://127.0.0.1:' + PORT + '/')
+
+# if __name__ == "__main__":
+#       app.config['TEMPLATES_AUTO_RELOAD']=True
+#       app.run(port=PORT, debug=True,use_reloader=True, host="0.0.0.0")
+#       Timer(1, open_browser).start()
