946161ad0ea9c933241545c57afeef25be831e75
Zenahr Barzani <zenmatica@gmail.com>
2020-06-19 00:49:53 +0200

    added fundamental check

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -1,24 +1,37 @@
 from wtforms import Form, IntegerField, FloatField
 from wtforms.validators import InputRequired, Length, ValidationError
+import re
 
 # ✅
 class weightValueForm(Form):
     weightValue = FloatField('Weight', validators=[InputRequired()])
 
     def validate_weightValue(self, weightValue):
-        regex_1 = r'\d\d\d.\d\d' # 000.00
-        regex_2 = r'\d\d\d.\d'   # 000.0
-        regex_3 = r'\d\d.\d\d'   # 00.00
-        regex_4 = r'\d\d.\d'     # 00.0
-        regex_5 = r'\d\d\d'      # 000
-        regex_5 = r'\d\d'        # 00
+        
         
 
         # sanitize , -> .
-        value = weightValue.replace(',','.')
+        if "," in weightValue and not isinstance(weightValue, int) or isinstance(weightValue, float):
+            value = weightValue.replace(',','.')
+
+        """regex for all possible numbers"""
+        regex_list = [
+            r'^\d\d\d.\d\d$', # 000.00
+            r'^\d\d\d.\d$',   # 000.0
+            r'^\d\d.\d\d$',   # 00.00
+            r'^\d\d.\d$',     # 00.0
+            r'^\d\d\d$',      # 000
+            r'^\d\d$'        # 00
+        ]
         
-        # If regex fails
-        raise ValidationError()
+        #regex
+        for regex in regex_list:
+            match = re.search(regex, value)
+            if match:
+                return value
+            else:
+                raise ValidationError("bad input")
+
 
 
 # ✅
@@ -33,3 +46,26 @@ class goalWeightForm(Form):
 # form.process(Amount='2.0')
 # print(form.validate())
 
+def validate_weightValue(weightValue):
+    """regex for all possible numbers"""
+    regex_list = [
+        r'^\d\d\d.\d\d$', # 000.00
+        r'^\d\d\d.\d$',   # 000.0
+        r'^\d\d.\d\d$',   # 00.00
+        r'^\d\d.\d$',     # 00.0
+        r'^\d\d\d$',      # 000
+        r'^\d\d$'        # 00
+    ]
+
+    # sanitize , -> .
+    value = weightValue.replace(',','.')
+
+    #regex
+    for regex in regex_list:
+        match = re.search(regex, value)
+        if match:
+            return value
+        else:
+            raise ValidationError("bad input")
+
+validate_weightValue(90)
