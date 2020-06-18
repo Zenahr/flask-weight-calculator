46830bccf38e69795c1222fdee2f04e720a595b3
Zenahr Barzani <zenmatica@gmail.com>
2020-06-19 00:29:01 +0200

    added regex array

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -1,9 +1,25 @@
 from wtforms import Form, IntegerField, FloatField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length, ValidationError
 
 # ✅
 class weightValueForm(Form):
-    weightValue = FloatField('Amount', validators=[InputRequired()])
+    weightValue = FloatField('Weight', validators=[InputRequired()])
+
+    def validate_weightValue(self, weightValue):
+        regex_1 = r'\d\d\d.\d\d' # 000.00
+        regex_2 = r'\d\d\d.\d'   # 000.0
+        regex_3 = r'\d\d.\d\d'   # 00.00
+        regex_4 = r'\d\d.\d'     # 00.0
+        regex_5 = r'\d\d\d'      # 000
+        regex_5 = r'\d\d'        # 00
+        
+
+        # sanitize , -> .
+        value = weightValue.replace(',','.')
+        
+        # If regex fails
+        raise ValidationError()
+
 
 # ✅
 class goalWeightForm(Form):
@@ -13,7 +29,7 @@ class goalWeightForm(Form):
 
 # Test ----------------------------------------------------------------------------
 
-form = weightValueForm()
-form.process(weightValueForm=2.0)
-form.validate()
-print(form.errors)
+# form = weightValueForm()
+# form.process(Amount='2.0')
+# print(form.validate())
+
